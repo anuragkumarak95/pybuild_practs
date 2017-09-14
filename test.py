8eb1418ba59445d3b36e4e840abcb1affbcc4cd4
Anurag Kumar <anuragkumarak95@gmail.com>
2017-09-14 23:26:06 +0530

    added test cases in test.py

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ import awesome
 class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(awesome.smile(), ":)")
+        self.assertEqual(awesome.smile(), ":)")
+        self.assertEqual(awesome.smile(), ":)")
+
 
 
 if __name__ == '__main__':
